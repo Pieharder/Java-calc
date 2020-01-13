d7b4a9bbdc63b393ddf2b6dce3d0d2a16f529e72
Brandan Sayarath and Geoff Goetz <geoff@>
2020-01-13 10:51:28 -0800

    add backend code to calculator

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+//Add
+var add = function(number1, number2) {
+    return number1 + number2;
+  };
+
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Plus:"));
+
+
+//Subtract
+var subtract = function(number1, number2) {
+return number1 - number2;
+  };
+
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Minus:"));
+  
+
+// Multiply
+var multiply = function(number1, number2) {
+  return number1 * number2;
+    };
+  
+    var number1 = parseInt(prompt("Enter a number:"));
+    var number2 = parseInt(prompt("Times:"));
+    
+
+// Divide
+var divide = function(number1, number2) {
+  return number1 / number2;
+    };
+  
+    var number1 = parseInt(prompt("Enter a number:"));
+    var number2 = parseInt(prompt("Divided by:"));
+    
+    alert(divide(number1,number2));
+
+
+  
